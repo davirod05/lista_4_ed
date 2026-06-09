1ef604f760c1503df02ed29226b0023b5221d6eb
Coffee <cauafergon@yahoo.com>
2026-06-09 17:22:37 -0300

    Adicionando main + readme, questões 7, 8 e 9

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        std::cout << "Usage: ./main prefix k" << std::endl;
+        std::cout << "Usage: ./app k prefix" << std::endl;
         return 1;
     }
 
-    std::string prefix = argv[1];
-    int k = std::atoi(argv[2]);
+    int k = std::atoi(argv[1]);
+    std::string prefix = argv[2];
 
     Trie trie;
     for (int i = 0; i < numberOfGames; i++) {
@@ -20,15 +20,14 @@ int main(int argc, char* argv[]) {
     std::vector<Game*> results = trie.autocomplete(prefix, k);
 
     if (results.empty()) {
-        std::cout << "Nenhum jogo encontrado para o prefixo \"" 
-        << prefix 
-        << "\"" 
-        << std::endl;
+        std::cout << "No results found" << std::endl;
         return 0;
     }
 
     for (int i = 0; i < (int)results.size(); i++) {
-        std::cout << results[i]->getTitle() << std::endl;
+        std::cout << results[i]->getTitle() << " | "
+                  << results[i]->getShortDescription() << " | "
+                  << results[i]->getPopularity() << std::endl;
     }
 
     return 0;
